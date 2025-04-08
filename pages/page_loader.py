4ffa356eb4f733018bde7efb46de63c8e245b770
Nguyễn Đức Tuân <nguyenductuan11122006@gmail.com>
2025-04-08 17:16:03 +0700

    Subdomain for both API and page-interface

--- pages/page_loader.py
+++ pages/page_loader.py
@@ -1,7 +1,10 @@
 from . import app
 
+from settings import SUBDOMAIN_API
+from settings import SUBDOMAIN_MAIN_PAGE
+
 from .blueprints import api_blueprint
 from .blueprints import main_blueprint
 
-app.register_blueprint(api_blueprint.blueprint, subdomain="api")
-app.register_blueprint(main_blueprint.blueprint, subdomain=None)
+app.register_blueprint(api_blueprint.blueprint, subdomain=SUBDOMAIN_API)
+app.register_blueprint(main_blueprint.blueprint, subdomain=SUBDOMAIN_MAIN_PAGE)
